3cb682454e3bc84497a894a86bea674672e64eb0
dudostka228 <iliakarpov1337@gmail.com>
2025-02-21 18:21:00 +0300

    4

--- index.ts
+++ index.ts
@@ -3,7 +3,18 @@ import "./global"
 import "./translations"
 export * from "C:/github.com/octarine-public/wrapper/wrapper/Imports"
 
-console.log("Hello world!");
-EventsSDK.on("GameStarted", () => {
-	console.log("Hello world!");
-})
+// console.log("Hello world!");
+// EventsSDK.on("GameStarted", () => {
+// 	console.log("Hello world!");
+// })
+
+console.log("Модуль загружен");
+
+export function init(ctx) {
+    if (ctx && ctx.chat && typeof ctx.chat.send === 'function') {
+        ctx.chat.send("1");
+    } else {
+        console.error("2");
+    }
+}
+
