056525e12029660174fb7140c3368ececfbfb96c
dudostka228 <iliakarpov1337@gmail.com>
2025-02-21 18:05:06 +0300

    cl2

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import "./global"
 import "./translations"
 export * from "C:/github.com/octarine-public/wrapper/wrapper/Imports"
 
-console.log("Hello world!")
+console.log("Hello world!");
 EventsSDK.on("GameStarted", () => {
-	console.log("Hello world!")
+	console.log("Hello world!");
 })
