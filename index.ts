ff137617f6c7a06e23e66558facdb1aceffb0d4f
dudostka228 <iliakarpov1337@gmail.com>
2025-02-21 18:24:57 +0300

    4

--- index.ts
+++ index.ts
@@ -1,19 +1,7 @@
 import { EventsSDK } from "github.com/octarine-public/wrapper/index"
 
-console.log("Hello world!");
 console.log("Hello world!")
-EventsSDK.on("GameStarted", () => {
-	console.log("Hello world!");
-})
 EventsSDK.on("GameStarted", () => {
 	console.log("Hello world!")
 })
-
-export function init(ctx) {
-    if (ctx && ctx.chat && typeof ctx.chat.send === 'function') {
-        ctx.chat.send("1");
-    } else {
-        console.error("2");
-    }
-}
 
