ba0c3c83fe4535691fe65cf9a9cb63cbfb76eb7b
dudostka228 <iliakarpov1337@gmail.com>
2025-02-21 17:47:33 +0300

    consolelog

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { EventsSDK } from "github.com/octarine-public/wrapper/index"
 import "./global"
 import "./translations"
+export * from "C:/github.com/octarine-public/wrapper/wrapper/Imports"
 
 console.log("Hello world!")
 EventsSDK.on("GameStarted", () => {
