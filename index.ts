a7c4380157b80264ed2f07a255dccb9f9b8b77e1
dudostka228 <iliakarpov1337@gmail.com>
2025-06-24 16:03:52 +0300

    1

--- index.ts
+++ index.ts
@@ -1,36 +1,61 @@
 import { 
 	EventsSDK,
-	Menu
+	Menu,
+	LocalPlayer,
+	EntityManager,
+	item_armlet,
+	TickSleeper,
+	GameState
  } from "github.com/octarine-public/wrapper/index"
 
-EventsSDK.on("GameStarted", () => {
-	console.log("Hello world!")
-})
 
-class MyMenu {
-	public readonly State: Menu.Toggle
-	private readonly slider: Menu.Slider
 
-	constructor() {
-		const entry = Menu.AddEntry("Armlet Abuse")
+ const Sleeper = new TickSleeper()
 
-		// const node = entry.AddNode("HP Count")	
-		// node.SortNodes = false
-
-		this.State = entry.AddToggle("Включить", true)
-		this.State.OnValue(t => {
-			console.log("scrpt activated:", t.value)
-		})
-
-		this.slider = entry.AddSlider("Слайдер с числами", 400, 1, 500, 0)
-
-		this.slider.IsHidden = false
-
-		this.State.OnValue((call) => {
-			this.slider.IsHidden = !call.value
-		})
-	}
-
-}
-
-new MyMenu()
+ const Entry = Menu.AddEntry("Armlet")
+ const ArmletNode = Entry.AddNode("Armlet Abuse")
+ ArmletNode.SortNodes = false
+ 
+ const ArmletState = ArmletNode.AddToggle("State", true)
+ const HPCount    = ArmletNode.AddSlider("HP Count", 400, 1, 500, 0)
+ 
+ EventsSDK.on("PostDataUpdate", delta => {
+   const me = LocalPlayer?.Hero
+   if (
+	 delta === 0 ||
+	 !ArmletState.value ||
+	 !me ||
+	 !me.IsAlive ||
+	 Sleeper.Sleeping
+   ) {
+	 return
+   }
+ 
+   const urnMod = me.Modifiers.find(
+	 m => m.Name === "modifier_item_urn_of_shadows_debuff"
+   )
+   if (!urnMod || urnMod.RemainingTime * 1000 > HPCount.value) {
+	 return
+   }
+ 
+   const armlet = EntityManager
+	 .GetEntitiesByClass(item_armlet)
+	 .find(a => a.Owner === me)
+   if (!armlet || !armlet.CanBeCasted()) {
+	 return
+   }
+ 
+   const lag     = (GameState.InputLag + GameState.IOLag) * 1000
+   const remMs   = (urnMod.RemainingTime % 1) * 1000 || 1000
+   const delayMs = Math.max(remMs - lag - 50, 0)
+ 
+   setTimeout(() => {
+	 me.CastToggle(armlet)
+	 me.CastToggle(armlet)
+	 Sleeper.Sleep(100)
+   }, delayMs)
+ })
+ 
+ EventsSDK.on("GameEnded", () => {
+   Sleeper.ResetTimer()
+ })
